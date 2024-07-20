3dfba3f0c8ad3af0829022ae49cb926c5383720e
Akshay Dinesh <akshay5632@gmail.com>
2024-07-20 11:56:47 +0530

    error handling complete

--- app/controllers/errorController.ts
+++ app/controllers/errorController.ts
@@ -1,5 +1,30 @@
 import { Request, Response, NextFunction } from "express";
 
+const sendErrorDev = (err: any, res: Response) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+    error: err,
+    stack: err.stack,
+  });
+};
+const sendErrorProd = (err: any, res: Response) => {
+  // Operational error: send message to client
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+    // Programming or other unknown errors: don't leak error details
+  } else {
+    console.error("Error - ", err);
+    res.status(500).json({
+      status: "error",
+      message: "Something went wrong!",
+    });
+  }
+};
+
 const globalErrorHandler = (
   err: any,
   req: Request,
@@ -9,10 +34,11 @@ const globalErrorHandler = (
   err.statusCode = err.statusCode || 500;
   err.status = err.status || "error";
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+  if (process.env.NODE_ENV === "development") {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === "production") {
+    sendErrorProd(err, res);
+  }
 };
 
 export default globalErrorHandler;
--- app/server.ts
+++ app/server.ts
@@ -2,6 +2,14 @@ import { createServer } from "http";
 import { roomWebSocketServers } from "./helpers/rooms.helper";
 import app from ".";
 
+process.on("uncaughtException", (err: any) => {
+  console.log("Unhandled exception! Shutting down...");
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
+});
+
 const server = createServer(app);
 
 server.on("upgrade", (request, socket, head) => {
@@ -20,4 +28,12 @@ server.on("upgrade", (request, socket, head) => {
 
 server.listen(8080, () => {
   console.log("Server is listening on port 8080");
+});
+
+process.on("unhandledRejection", (err: any) => {
+  console.log("Unhandled rejection! Shutting down...");
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
 });
